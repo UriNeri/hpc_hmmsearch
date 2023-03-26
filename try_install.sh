1afa8b7a3e929acd23c4ea391660aa2b038d7b38
UriNeri <41674327+UriNeri@users.noreply.github.com>
2023-03-26 11:38:57 +0300

    Create try_install.sh

---
+++ try_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ~
+mkdir scripts
+cd scripts
+wget http://eddylab.org/software/hmmer/hmmer-3.3.2.tar.gz
+extract hmmer-3.3.2.tar.gz
+cd hmmer-3.3.2
+cd src
+wget https://raw.githubusercontent.com/Larofeticus/hpc_hmmsearch/master/hpc_hmmsearch.c
+cd ..
+#autoconf
+./configure
+make V=1 | grep "hmmsearch"
+# copy paste, add the hpc_ prefix etc etc. See the actual repo and ths fork for info e.g. openmp and so on.
+# Should look something like:
+cd src
+gcc -O3 -pthread     -DHAVE_CONFIG_H  -I../easel -I../libdivsufsort -I../easel -I. -I. -o hpc_hmmsearch.o -c hpc_hmmsearch.c 
+gcc -O3 -pthread     -DHAVE_CONFIG_H  -L../easel -L./impl_sse -L../libdivsufsort -L. -o hpc_hmmsearch hpc_hmmsearch.o -lhmmer -leasel -ldivsufsort     -lm
