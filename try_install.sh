11b8a2f4070173a994d4e17fa1d6fb4970eefbd3
UriNeri <41674327+UriNeri@users.noreply.github.com>
2023-03-27 13:14:03 +0300

    Update try_install.sh
    add openmp flag

--- try_install.sh
+++ try_install.sh
@@ -13,6 +13,6 @@ cd ..
 make V=1 | grep "hmmsearch"
 # copy paste, add the hpc_ prefix etc etc. See the actual repo and ths fork for info e.g. openmp and so on.
 # Should look something like:
-cd src
-gcc -O3 -pthread     -DHAVE_CONFIG_H  -I../easel -I../libdivsufsort -I../easel -I. -I. -o hpc_hmmsearch.o -c hpc_hmmsearch.c 
-gcc -O3 -pthread     -DHAVE_CONFIG_H  -L../easel -L./impl_sse -L../libdivsufsort -L. -o hpc_hmmsearch hpc_hmmsearch.o -lhmmer -leasel -ldivsufsort     -lm
+  cd src
+  gcc -O3 -pthread     -DHAVE_CONFIG_H  -I../easel -I../libdivsufsort -I../easel -I. -I. -o hpc_hmmsearch.o -c hpc_hmmsearch.c -openmp
+  gcc -O3 -pthread     -DHAVE_CONFIG_H  -L../easel -L./impl_sse -L../libdivsufsort -L. -o hpc_hmmsearch hpc_hmmsearch.o -lhmmer -leasel -ldivsufsort     -lm -openmp
